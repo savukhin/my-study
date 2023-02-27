bab0432884fa52f1e9497b83ce3ff1a21e8220b2
savukhin <savukhin@gmail.com>
2023-02-27 19:40:58 +0300

    Added task 15 good

--- 6_Semester/Java/Lab_1/task_15/Main.java
+++ 6_Semester/Java/Lab_1/task_15/Main.java
@@ -1,48 +1,63 @@
-/*
- * Write a program that stores Pascal’s triangle up to a given n in an
-ArrayList<ArrayList<Integer>>
- */
-
 package task_15;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
-    public static String repeat(int count, String with) {
-        return new String(new char[count]).replace("\0", with);
-    }
-    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.close();
-        
-        ArrayList<ArrayList<Integer>> pascalTriangle = new ArrayList<ArrayList<Integer>>();
-        
-        for (int i = 0; i < n; i++) {
-            ArrayList<Integer> row = new ArrayList<Integer>();
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    row.add(1);
-                } else {
-                    row.add(pascalTriangle.get(i - 1).get(j - 1) + pascalTriangle.get(i - 1).get(j));
+        int[][] array = new int[4][4];
+
+        for(int i=0; i< array.length; i++){
+            for(int j=0; j<array[i].length; j++){
+                System.out.println("Введите целочисленное значение array[" + i + "][" + j + "]:");
+                Scanner in = new Scanner(System.in);
+                while (!in.hasNextInt()) {
+                    System.out.println("Введите корректное значение!");
+                    in.next();
                 }
+                array[i][j]= in.nextInt();
             }
-            pascalTriangle.add(row);
         }
-        
-
-        for (int i = 0; i < pascalTriangle.size(); i++) {
-            ArrayList<Integer> row = pascalTriangle.get(i);
-
-            System.out.print(repeat(n - i, " "));
-
-            for (Integer element : row) {
-                System.out.print(element + " ");
+        //check column
+        int sum=0;
+        int[] array_int = new int[10];
+        //string
+        for(int i=0; i< array.length; i++){
+            for (int j=0; j<array[i].length; j++){
+                sum+=array[i][j];
             }
-            System.out.println();
+            array_int[i] = sum;
+            sum=0;
+        }
+        //column
+        for(int i=0; i< array.length; i++){
+            for (int j=0; j<array.length; j++){
+                sum+=array[i][j];
+            }
+            array_int[i+array.length] = sum;
+            sum=0;
+        }
+        //diagon
+        for(int i=0; i<array.length; i++){
+            sum+=array[i][i];
+        }
+        array_int[8] = sum;
+        sum=0;
+        for(int i=(array.length-1); i>=0; i--){
+            sum+=array[i][i];
+        }
+        array_int[9] = sum;
+
+        //check
+        int flag = 0;
+        for(int j=0; j<9; j++){
+            if(array_int[j]!= array_int[j+1]){
+                flag =1;
+            }
+        }
+        if(flag==1){
+            System.out.println(false);
+        }else {
+            System.out.println(true);
         }
     }
 }
